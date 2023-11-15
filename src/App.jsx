ac4a406d5205826897cea067294c0a5a259952c5
ShahMuhammadQureshi <123717044+ShahMuhammadQureshi@users.noreply.github.com>
2023-11-15 07:15:49 +0000

    Firstcommit

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,12 @@ import "./styles.css";
  * but its stupid that you dont have a resume make one ASAP
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Shah Muhammad Qureshi",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "shahmuhammadquresih@gmail.com",
+  gitHub: "Shah_419",
+  instagram: "Shah_419",
+  linkedIn: "Shah_419",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
